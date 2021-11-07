b385c1a06503feecc0a836c22a896c3527d36bc1
Andrey Zakharov <andrey.zakharov@egym.de>
2021-11-07 12:18:03 +0100

    Fixes broken test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meeting_rooms/app/app.dart';
-import 'package:meeting_rooms/counter/counter.dart';
+import 'package:meeting_rooms/app/view/app_page.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders AppPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(AppPage), findsOneWidget);
     });
   });
 }
