6e43c1516d4e50eab0eb9832aad7cec0b2ba1447
Andrey Zakharov <andrey.zakharov@egym.com>
2022-11-20 12:46:27 +0100

    Clarifies what application is doing on the main not authorized page

--- lib/app/view/application_information.dart
+++ lib/app/view/application_information.dart
@@ -6,9 +6,24 @@ class ApplicationInformation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('This is small application that tries to '
-        'address the situation when you urgently need a meeting room, '
-        'but it is hard to find one in Google Calendar. '
-        'Just click on `Sign in with Google` in the right top corner.');
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'This is a small application which provides an overview over free '
+            'and busy meetings rooms that are available to book in your Google '
+            'Calendar.',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          const SizedBox(height: 24),
+          Text(
+            'Just click on `Sign in with Google` in the right top corner.',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ],
+      ),
+    );
   }
 }
